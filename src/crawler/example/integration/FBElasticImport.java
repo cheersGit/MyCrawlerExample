bc8509620cff366313f74108f830d6450f7d83b5
cheersGit <moscowroses@yahoo.com.tw>
2017-12-10 10:22:42 +0800

    2017.12.09 TestHighwayStaticInfo

--- src/crawler/example/integration/FBElasticImport.java
+++ src/crawler/example/integration/FBElasticImport.java
@@ -12,16 +12,16 @@ public class FBElasticImport {
 
     static String elasticHost = "dyn.gibar.co" ;
     static String elasticPort = "9200" ;
-    static String elasticIndex = "abola2";
+    static String elasticIndex = "cheersgit";
     static String elasticIndexType = "data";
-    static String pageName = "JudgeAd";
+    static String pageName = "appledaily.tw";
     // 2017-09-02
-    static long start = 1504363907;
+    static long start = 1512871850 ;
     // 往前抓抓取日期數
     static int days = 10;
     // 每日抓取文章上限 (上限1000)
     static int maxPosts = 10;
-    static String access_token = "EAACEdEose0cBAAuJhUlaZCuuqYnzXXXKPGY7Es4KZCLBLpnrIdCWjutfSl1AFZAdR3pFnF6PdrfmaU2EwyllSxhKAd7ZCIGfxZCeg8VkdspelNr9vVWIZCeBRnU5KUlZAPh9zamyT836FmdCadDfzwFILwR6hdMse7irLpIto5cnUnqSV4MtEAA2Hf8slFdXZBS489aSanZCYLAZDZD";
+    static String access_token = "EAACEdEose0cBAPZCumC1LYSxj1QJfcHHx7BnjvXyPqzWn9ZCcDhMQMnv3Mp2eyTIFZCCfaK3ZAZBDh2wdY38ZBMFk9RDndYfJGWM9WbIDClXfcuEtV3IltWaPncQZCiUFp27jnaEIWAHTi7h3vjNZCymVp0PU0sBO6L0HVvlVQ8bqJecgnDNlWY7m5y3QKfBEZAYmAA1ZBvZBqG3gZDZD";
 
     public static void main(String[] args) {
 
