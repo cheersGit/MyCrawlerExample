8a53c75585ade6a0dfc0e667011e14982d7cb0ba
cheersGit <moscowroses@yahoo.com.tw>
2017-12-10 12:09:16 +0800

    2017.12.09 TestHighwayStaticInfo

--- src/crawler/example/PttReExample.java
+++ src/crawler/example/PttReExample.java
@@ -22,16 +22,13 @@ public class PttReExample {
 		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
 		// 遠端資料路徑
-		//String uri = "https://pm25.lass-net.org/data/last-all-airbox.json";
-		//String uri="http://www.twse.com.tw/exchangeReport/MI_INDEX?response=html&date=20171204&type=MS";
+
 		String uri="https://www.ptt.cc/bbs/CVS/M.1512786866.A.DF1.html";
-
-
-		Elements jsoup=CrawlerPack.start()
+    	Elements jsoup=CrawlerPack.start()
 				.addCookie("over18","1")
 				.getFromHtml(uri)
 				.select("#main-content");
-		System.out.println(jsoup.select("span,div").remove());
+		System.out.println(jsoup.select("span,div").remove().text());
 		//.setRemoteEncoding("big5") // 設定遠端資料文件編碼
 
 
